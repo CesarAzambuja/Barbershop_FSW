e0236d487cceb7042437dfc691eb9bc99b3f2f76
cesarazambuja <dev.azambuja@outlook.com>
2024-02-01 22:56:45 -0300

    Add components services in Barbershops

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,16 +1,19 @@
-import { Button } from "@/app/_components/ui/button";
+
 import { db } from "@/app/_lib/prisma";
-import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
-import Image from "next/image";
 import BarbershopInfo from "./_components/barbershop-inf";
+import ServiceItem from "./_components/service-item";
+import { Key } from "react";
+
+
 
 interface BarbershopDetailsPageProps {
     params: {
         id?: string;
+        
     };
 };
 
-const BarbershopDetailsPage = async ({params}: BarbershopDetailsPageProps) => {
+const BarbershopDetailsPage = async ({ params }: BarbershopDetailsPageProps) => {
 
     if (!params.id) {
         // TODO: Redirecionar para a homepage   
@@ -19,15 +22,29 @@ const BarbershopDetailsPage = async ({params}: BarbershopDetailsPageProps) => {
     const barbershop = await db.barbershop.findUnique({
         where: {
             id: params.id,
+        },
+
+        include: {
+            services: true,
         }
-    });
+    })
 
     if (!barbershop){
         // TODO: Redirecionar para a homepage   
         return null
     }
 
-    return <BarbershopInfo barbershop={barbershop} />
-}
+    return (
+    <div>
+        <BarbershopInfo barbershop={barbershop} />
+
+       <div className="px-5 flex flex-col gap-4 py-6">
+       {barbershop.services.map((service) => (
+            <ServiceItem key={service.id} service={service} />
+        ))}
+       </div>
+    </div>
+    );
+};
  
 export default BarbershopDetailsPage;
