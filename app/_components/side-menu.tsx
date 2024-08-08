e57e6cc7e567e67061a1262442226bc3fe503849
César Oliveira <cesar.oliveira@takelog.com.br>
2024-08-07 23:29:16 -0300

    Chore: add nex auth setup

--- app/_components/side-menu.tsx
+++ app/_components/side-menu.tsx
@@ -6,63 +6,81 @@ import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "./ui
 import { signIn, signOut, useSession } from "next-auth/react";
 import { Avatar, AvatarImage } from "./ui/avatar";
 import Link from "next/link";
+import { Dialog, DialogContent, DialogTrigger, DialogHeader, DialogTitle, DialogDescription } from "./ui/dialog";
+import Image from "next/image";
 const SideMenu = () => {
-        const { data } = useSession();
-    
-        const handleLogoutClick = () => signOut();
-        const handleLoginClick = () => signIn("google");
-    return ( 
+    const { data } = useSession();
+
+    const handleLogoutClick = () => signOut();
+    const handleLoginClick = () => signIn("google");
+    return (
 
         <>
             <SheetHeader className="text-left border-b border-solid border-secondary p-5">
-                            <SheetTitle>Menu</SheetTitle>
-                        </SheetHeader>
+                <SheetTitle>Menu</SheetTitle>
+            </SheetHeader>
 
-                        {data?.user ? (
-                            <div className="flex justify-between px-5 py-6 items-center">
-                                <div className="flex items-center gap-3 ">
-                                    <Avatar>
-                                        <AvatarImage src={data.user?.image ?? ""}/>
-                                    </Avatar>
+            {data?.user ? (
+                <div className="flex justify-between px-5 py-6 items-center">
+                    <div className="flex items-center gap-3 ">
+                        <Avatar>
+                            <AvatarImage src={data.user?.image ?? ""} />
+                        </Avatar>
 
-                                    <h2 className="font-bold">{data.user.name}</h2>
-                                </div>
-                                <Button  variant="secondary" size="icon">
-                                    <LogOutIcon onClick={handleLogoutClick}/>
-                                </Button>   
-                            </div>
-                        ) : (
-                            <div className="flex flex-col px-5 py-6 gap-3">
-                                <div className="flex items-center gap-2 ">
-                                    <UserIcon size={32}/>
-                                    <h2 className="font-bold">Olá, faça seu login!</h2>
-                                </div>
-                                <Button variant="secondary" className="w-full" onClick={handleLoginClick}>
-                                <LogInIcon className="mr-2" size={18}/>
-                                Fazer Login</Button> 
-                            </div>
-                        )}
-
-                        <div className="flex flex-col gap-3 px-5">
-                            <Button variant="outline" className="justify-start" asChild>
-                                <Link href="/">
-                                    <HomeIcon size={18} className="mr-2"/>
-                                    Início
-                                </Link>
-                            </Button>
-
-                            {data?.user && (
-                                <Button variant="outline" className="justify-start" asChild>
-                                    <Link href="/bookings">
-                                        <CalendarIcon size={18} className="mr-2"/>
-                                        Meus Agendamentos
-                                    </Link>
-                                </Button>
-                            )}
+                        <h2 className="font-bold">{data.user.name}</h2>
+                    </div>
+                    <Button variant="secondary" size="icon">
+                        <LogOutIcon onClick={handleLogoutClick} />
+                    </Button>
+                </div>
+            ) : (
+                <div className="flex flex-col px-5 py-6 gap-3">
+                    <div className="flex items-center gap-2 ">
+                            <UserIcon size={32} />
+                            <h2 className="font-bold">Olá, faça seu login!</h2>
                         </div>
-                            
+                        <Dialog>
+                            <DialogTrigger>
+                                <Button className="w-full" >
+                                    <LogInIcon className="mr-2" size={18} />
+                                    Fazer Login</Button>
+                            </DialogTrigger>
+                            <DialogContent className="w-[90%]">
+                                <DialogHeader>
+                                    <DialogTitle>Faça seu login</DialogTitle>
+                                    <DialogDescription>
+                                        Conecte-se usando sua conta Google
+                                    </DialogDescription>
+                                </DialogHeader>
+                                <Button variant="outline" className="gap-2 font-bold">
+                                    <Image alt="Login com Google" src="/google.svg" width={18} height={18} />
+                                    Google
+                                </Button>
+                            </DialogContent>
+                        </Dialog>
+                </div>
+            )}
+
+            <div className="flex flex-col gap-3 px-5">
+                <Button variant="outline" className="justify-start" asChild>
+                    <Link href="/">
+                        <HomeIcon size={18} className="mr-2" />
+                        Início
+                    </Link>
+                </Button>
+
+                {data?.user && (
+                    <Button variant="outline" className="justify-start" asChild>
+                        <Link href="/bookings">
+                            <CalendarIcon size={18} className="mr-2" />
+                            Meus Agendamentos
+                        </Link>
+                    </Button>
+                )}
+            </div>
+
         </>
-     );
+    );
 }
- 
+
 export default SideMenu;
